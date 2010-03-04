ad24cb56be6225791905481f8c06aa8591a08b09
Adrian Sampson <asampson@cs.washington.edu>
2010-03-03 16:26:55 -0800

    allow constructor annotation; correctly initialize shadow fields written in super constructors

--- src/oshajava/annotation/Reader.java
+++ src/oshajava/annotation/Reader.java
@@ -3,7 +3,7 @@ package oshajava.annotation;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD})
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
 public @interface Reader {
 	/**
 	 * List of IDs of channels from which the annotated method reads.
