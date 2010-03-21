d1df49bf403ed31135961cdfebfac195b42b11c6
Benjamin Wood <bpw@cs.washington.edu>
2010-03-21 01:26:38 -0700

    profile reading code, scalability/slowdown grapher, jgf profiles

---
+++ prof/graph_scalability.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+from pychart import *
+import prof
+import sys
+
+prof.configPychart(name="scalability", color=False)
+
+nthreads = 8
+
+options = {
+    "arrayIndexStates" : "false", 
+    "objectStates" : "false", 
+    "profile" : "false", 
+    "arrayCacheSize" : "16", 
+    "lockCacheSize" : "4", 
+    "traces" : "false", 
+    "record" : "false", 
+    "create" : "false", 
+    "instrumentFullJDK" : "false", 
+    "bytecodeDump" : "false", 
+    "verify" : "false", 
+    "preVerify" : "false", 
+    "frames" : "false"
+}
+
+def jgfName(name):
+    # remove "JGF" from head, "BenchSize_" from end
+    return name if not name.startswith("JGF") else name[3:-10]
+
+thread_profs = prof.partition(lambda p: int(p["options"]["profileExt"].split("-")[1]),
+                             prof.loadAll(sys.argv[1:], 
+                                          filename_filter=(lambda fn: not fn.endswith("warmup.py")),
+                                          prof_filter=(lambda p: prof.matchOptions(options, p))))
+
+def slowdown((name, profiles)):
+    oshajava, java = prof.bisect(lambda p: prof.matchOptions({"noInstrument" : "false"}, p), profiles)
+    ojtimes = map(prof.getRuntime, oshajava)
+    jtimes = map(prof.getRuntime, java)
+    return (name, sum(ojtimes) / sum(jtimes))
+
+made_canvas = False
+
+i = 0
+for t,profs in thread_profs:
+    sd = map(slowdown, prof.partition(lambda p: jgfName(p["mainClass"]), profs))
+    if not made_canvas:
+        canvas = area.T(x_coord = category_coord.T(sd, 0),
+                x_axis=axis.X(label="Benchmarks", format="/a60%s"),
+                y_axis=axis.Y(label="OshaJava Slowdown (x)", tic_interval=5),
+                y_grid_interval=5,
+                size=(240,110))
+        made_canvas = True
+    canvas.add_plot(bar_plot.T(label=str(t), data=sd, cluster=(i,len(thread_profs))))
+    i += 1
+
+canvas.draw()
---
+++ prof/prof.py
@@ -0,0 +1,124 @@
+import os
+
+def load(file):
+    f = open(file, 'r')
+    prof = eval(f.read())
+    f.close()
+    if "mainClass" not in prof["options"]:
+        prof["options"]["mainClass"] = os.path.basename(file).split("-")[0]
+    prof["mainClass"] = prof["options"]["mainClass"]
+    return prof
+
+def loadAll(files, filename_filter=(lambda x: True), prof_filter=(lambda x: True)):
+    return filter(prof_filter, map(load, filter(lambda f: filename_filter(os.path.basename(f)), files)))
+
+def toTuple(fields, profile):
+    return tuple([ profile[f] for f in fields ])
+
+def mapToTuples(fields, profiles):
+    return map(lambda p: toTuple(fields, p), profiles)
+
+def mapToField(field, profiles):
+    return map(lambda p: p[field], profiles)
+
+def matchOptions(options, profile):
+    for k,v in options.iteritems():
+        if profile["options"][k] != v:
+            return False
+    return True
+
+def splitByConfig(profiles):
+    configs = {}
+    for p in profiles:
+        key = p["options"]
+        if key in configs:
+            configs[key].append(p)
+        else:
+            configs[key] = [p]
+    return configs
+
+def bisect(filt, list):
+    y,n = [],[]
+    for i in list:
+        if filt(i):
+            y.append(i)
+        else:
+            n.append(i)
+    return (y,n)
+
+def partition(mapper, list):
+    parts = {}
+    for i in list:
+        key = mapper(i)
+        if key in parts:
+            parts[key].append(i)
+        else:
+            parts[key] = [i]
+    return sorted(parts.items())
+
+
+
+## accessors
+def getRuntime(prof):
+    return prof["Premain to fini time"]
+
+def getInstime(prof):
+    return prof["Instrumentation time"]
+
+def getPeakMem(prof):
+    return prof["Memory peak"]
+
+## profile accessors
+def getReads(prof):
+    return prof["All field reads"] + prof["All array reads"]
+
+def getCommReads(prof):
+    return prof["Communicating field reads"] + prof["Communicating array reads"]
+
+def getChecks(prof):
+    return prof["All field reads"] + prof["All array reads"] + prof["All acquires"]
+
+def getStackWalks(prof):
+    return prof["Full stack walks"]
+
+def getSlowComms(prof):
+    return prof["Communicating field read slow path"] +  prof["Communicating array read slow path"] +  prof["Communicating acquire slow path"]
+
+def getSlowMemoHits(prof):
+    return getSlowComms(prof) - getStackWalks(prof)
+
+def getComms(prof):
+    return prof["Communicating field reads"] +  prof["Communicating array reads"] +  prof["Communicating acquires"]
+
+def getFastMemoHits(prof):
+    return getComms(prof) - getSlowComms(prof)
+
+def getThreadLocalHits(prof):
+    return getChecks(prof) - getComms(prof)
+
+def getThreadLocalRate(prof):
+    return float(getThreadLocalHits(prof)) / float(getChecks(prof))
+
+def getCommRate(prof):
+    return float(getComms(prof)) / float(getChecks(prof))
+
+def getFastMemoHitRate(prof):
+    return flaot(getFastMemoHits(prof)) / float(getChecks(prof))
+
+def getSlowMemoHitRate(prof):
+    return float(getSlowMemoHits(prof)) / float(getChecks(prof))
+
+def getStackWalkRate(prof):
+    return float(getStackWalks(prof)) / float(getChecks(prof))
+
+
+
+######### Graph config ########
+
+def configPychart(name=None,color=True):
+    assert name != None
+    import pychart
+    pychart.theme.get_options()
+    pychart.theme.use_color = color
+    pychart.theme.output_file = name + ".pdf"
+    pychart.theme.reinitialize()
