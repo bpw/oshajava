6ce89782188f13fd170f38838d5a49421e4cbf61
Adrian Sampson <asampson@cs.washington.edu>
2010-03-22 17:47:27 -0700

    fix bench names for module counter

--- prof/count_modules.py
+++ prof/count_modules.py
@@ -6,17 +6,6 @@ import static
 import sys
 import os
 
-JGF_NAMES = {
-    'crypt': 'Crypt',
-    'lufact': 'LUFact',
-    'moldyn': 'MolDyn',
-    'montecarlo': 'MonteCarlo',
-    'raytracer': 'RayTracer',
-    'sor': 'SOR',
-    'series': 'Series',
-    'sparsematmult': 'SparseMatmult',
-}
-
 statses = static.load_all(sys.argv[1:])
 
 out = []
@@ -32,7 +21,7 @@ for name, stats in statses:
     
     avg = float(methods-inlined) / modules if modules else 0
     
-    out.append((JGF_NAMES.get(name, name), modules, avg))
+    out.append((static.BENCH_NAMES[name], modules, avg))
     all_uninlined += methods - inlined
     all_modules += modules
 print 'Num nonempty modules, average uninlined methods per nonempty module:', out
