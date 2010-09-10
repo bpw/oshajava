514f83a7b003f7499c3e76b1ea52a953701bf91a
Benjamin Wood <bpw@cs.washington.edu>
2010-09-09 21:17:50 -0700

    annotation tweak

--- src/oshajava/annotation/Groups.java
+++ src/oshajava/annotation/Groups.java
@@ -3,7 +3,7 @@ package oshajava.annotation;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
-//@Target({ElementType.PACKAGE, ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR})
+@Target({ElementType.PACKAGE, ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR})
 public @interface Groups {
 
 	/**
