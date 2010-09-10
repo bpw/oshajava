ed84cc9d079e8496259e9377f1857cca0f893a2c
Benjamin Wood <bpw@cs.washington.edu>
2010-09-09 20:33:28 -0700

    A cleaner revision, still work to go.

--- src/oshajava/sourceinfo/SpecProcessor.java
+++ src/oshajava/sourceinfo/SpecProcessor.java
@@ -23,6 +23,7 @@ import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.TypeParameterElement;
+import javax.lang.model.element.UnknownElementException;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.NestingKind;
@@ -61,9 +62,9 @@ public class SpecProcessor extends AbstractProcessor {
 	private final Map<String, ModuleSpecBuilder> modules = new HashMap<String, ModuleSpecBuilder>();
 //	private final Map<String, String> packageToModule = new HashMap<String, String>();
 	private final Map<Element, ModuleSpecBuilder> processedModules = new HashMap<Element, ModuleSpecBuilder>();
-	private final ModuleScanner moduleScanner = new ModuleScanner();
-	private final GroupScanner groupScanner = new GroupScanner();
-	private final GroupMembershipScanner groupMembershipScanner = new GroupMembershipScanner();
+	private final ModuleTraversal moduleScanner = new ModuleTraversal();
+	private final GroupTraversal groupScanner = new GroupTraversal();
+	private final CommTraversal groupMembershipScanner = new CommTraversal();
 	
 //	private final Set<ModuleSpecBuilder> changed = new HashSet<ModuleSpecBuilder>();
 	
@@ -101,195 +102,313 @@ public class SpecProcessor extends AbstractProcessor {
 		} else {
 			// First, establish modules.
 			for (final Element e : elements) {
-				moduleScanner.scan(e);
+				moduleScanner.traverse(e);
 			}
 			// Second, establish groups.
 			for (final Element e : elements) {
-				groupScanner.scan(e);
+				groupScanner.traverse(e);
 			}			
 			// Third, establish readers and writers in groups, noncomm, and inline.
 			for (final Element e : elements) {
-				groupMembershipScanner.scan(e);
+				groupMembershipScanner.traverse(e);
 			}
 		}
 		// Let other processors see the annotations.
 		return false;
 	}
 	
-	/**
-	 * Convenience...
-	 * @author bpw
-	 *
-	 * @param <R>
-	 * @param <P>
-	 */
-	class MyElementScanner<R,P> extends ElementScanner6<R, P> {
-		@Override
-		public R visitExecutable(ExecutableElement e, P p) { return null; }
-		@Override
-		public final R visitVariable(VariableElement e, P p) { return null; }
-		@Override
-		public final R visitTypeParameter(TypeParameterElement e, P p) { return null; }
-	}
-
-	/**
-	 * Scans Element tree for module membership declarations.
-	 * @author bpw
-	 *
-	 */
-	class ModuleScanner extends MyElementScanner<ModuleSpecBuilder, ModuleSpecBuilder> {
-		@Override
-		public ModuleSpecBuilder scan(Element e, ModuleSpecBuilder p) {
-			if (! processedModules.containsKey(e)) {
-				final Module memberDecl = e.getAnnotation(Module.class);
-				final ModuleSpecBuilder module;
-				if (memberDecl != null) {
-					module = getModuleByName(memberDecl.value());
-					processedModules.put(e, module);
-					super.scan(e, module);
-					return module;
+	abstract class Traversal {
+		
+		protected abstract boolean done(Element e);
+		/**
+		 * Invariant: when handle(e) is called, either parent == null or all of e's ancestors have been handled already.
+		 * @param e
+		 */
+		protected abstract void handle(Element e);
+		
+		public void traverse(Element e) {
+			if (!done(e)) {
+				// If this element is not already done:
+				final Element parent = e.getEnclosingElement();
+				if (parent != null && !done(parent)) {
+					// Parent is unlabeled: traverse from parent.
+					traverse(parent);
 				} else {
-					module = super.scan(e, p);
-					processedModules.put(e, module);
+					handle(e);
+					// Traverse children.
+					for (Element child : e.getEnclosedElements()) {
+						traverse(child);
+					}
 				}
-				return module;
 			}
-			return processedModules.get(e);
+		}
+	}
+	
+	abstract class LabelingTraversal<R> extends Traversal implements ElementVisitor<R,Object> {
+		
+		protected boolean done(Element e) {
+			return getLabel(e) != null;
+		}
+		protected abstract R getLabelFromAnnotation(Element e);
+		protected abstract void setLabel(Element e, R r);
+		protected abstract R getLabel(Element e);
+		
+		/**
+		 * Merge an elements label with that inherited from its parent.
+		 * Default behavior is to overwrite the inherited label with the local label if the
+		 * local label exists.
+		 * 
+		 * @param label
+		 * @param parentLabel
+		 * @return
+		 */
+		protected R mergeLabels(R label, R parentLabel) {
+			return label != null ? label : parentLabel;
 		}
 		
-		@Override
-		public ModuleSpecBuilder visitPackage(PackageElement pkg, ModuleSpecBuilder p) {
-			return getModuleByName(pkg.isUnnamed() ? ModuleSpec.DEFAULT_NAME : pkg.getQualifiedName() + "." + ModuleSpec.DEFAULT_NAME);
+		/**
+		 * Handle an element e.
+		 */
+		public void handle(Element e) {
+			// If this element is not already done:
+			final R label = getLabelFromAnnotation(e);
+			final Element parent = e.getEnclosingElement();
+			// Merge the label of this element and its parent.
+			final R mergedLabel = mergeLabels(label, parent == null ? null : getLabel(parent));
+			// If this is still empty, generate a default label.
+			setLabel(e, mergedLabel != null ? mergedLabel : visit(e));
 		}
 
+		public R visitPackage(PackageElement e, Object _) { throw new IllegalStateException("package " + e.getQualifiedName()); }
+		public R visitType(TypeElement e, Object _) { throw new IllegalStateException("type " + e.getQualifiedName() + " parent " +
+				e.getEnclosingElement() + " parent done " + done(e.getEnclosingElement()) + " parent label " + getLabel(e.getEnclosingElement())); }
+		public R visitVariable(VariableElement e, Object _) { throw new IllegalStateException("var"); }
+		public R visitExecutable(ExecutableElement e, Object _) { throw new IllegalStateException("method " + e.getSimpleName()); }
+		public R visitTypeParameter(TypeParameterElement e, Object _) { throw new IllegalStateException("type param"); }
+
+		public R visit(Element e, Object p) {
+			return e.accept(this, p);
+		}
+
+		public R visit(Element e) {
+			return visit(e, null);
+		}
+
+		public R visitUnknown(Element e, Object p) {
+			throw new UnknownElementException(e, p);
+		}
+		
+}
+
+	
+	class ModuleTraversal extends LabelingTraversal<ModuleSpecBuilder> {
+
 		@Override
-		public ModuleSpecBuilder visitType(TypeElement e, ModuleSpecBuilder p) {
-			if (p == null) {
-				p = scan(processingEnv.getElementUtils().getPackageOf(e));
+		protected ModuleSpecBuilder getLabelFromAnnotation(Element e) {
+			final Module memberDecl = e.getAnnotation(Module.class);
+			if (memberDecl != null) {
+				return getModuleByName(memberDecl.value());
+			} else {
+				return null;
 			}
-			super.visitType(e, p);
-			return p;
 		}
 
 		@Override
-		public ModuleSpecBuilder visitExecutable(ExecutableElement e, ModuleSpecBuilder p) {
-			return p;
+		protected void setLabel(Element e, ModuleSpecBuilder r) {
+			processedModules.put(e, r);
+		}
+
+		@Override
+		protected ModuleSpecBuilder getLabel(Element e) {
+			return e == null ? null : processedModules.get(e);
+		}
+
+		@Override
+		public ModuleSpecBuilder visitPackage(PackageElement e, Object _) {
+			return getModuleByName(e.isUnnamed() ? ModuleSpec.DEFAULT_NAME : e.getQualifiedName() + "." + ModuleSpec.DEFAULT_NAME);
 		}
 
 	}
 	
-	class GroupScanner extends MyElementScanner<Object, Object> {
-
-		private final Set<Element> processedGroups = new HashSet<Element>();
+	class GroupTraversal extends Traversal {
+		private final HashSet<Element> done = new HashSet<Element>();
 
 		@Override
-		public Object scan(Element e, Object p) {
-			if (! processedGroups.contains(e)) {
-				processedGroups.add(e);
-				final ModuleSpecBuilder module = processedModules.get(e);
-				// Handle any single group declarations.
-				addGroup(module, e.getAnnotation(Group.class));
-				addGroup(module, e.getAnnotation(InterfaceGroup.class));
-				// Handle multiple group declarations.
-				Groups groupsAnn = e.getAnnotation(Groups.class);
-				if (groupsAnn != null) {
-					for (Group groupAnn : groupsAnn.communication()) {
-						addGroup(module, groupAnn);
-					}
-					for (InterfaceGroup iGroupAnn : groupsAnn.intfc()) {
-						addGroup(module, iGroupAnn);
-					}
-				}
-				super.scan(e, p);
-			}
-			return null;
+		protected boolean done(Element e) {
+			return done.contains(e);
 		}
-		
+
+		@Override
+		protected void handle(Element e) {
+			final ModuleSpecBuilder module = processedModules.get(e);
+			// Handle any single group declarations.
+			addGroup(module, e.getAnnotation(Group.class));
+			addGroup(module, e.getAnnotation(InterfaceGroup.class));
+			// Handle multiple group declarations.
+			Groups groupsAnn = e.getAnnotation(Groups.class);
+			if (groupsAnn != null) {
+				for (Group groupAnn : groupsAnn.communication()) {
+					addGroup(module, groupAnn);
+				}
+				for (InterfaceGroup iGroupAnn : groupsAnn.intfc()) {
+					addGroup(module, iGroupAnn);
+				}
+			}
+			done.add(e);
+		}
+
 	}
-	
-	class GroupMembershipScanner extends MyElementScanner<Object, Object> {
 
-		private final Set<Element> processedGroupMemberships = new HashSet<Element>();
-
-		@Override
-		public Object scan(Element e, Object p) {
-			if (!processedGroupMemberships.contains(e)) {
-				processedGroupMemberships.add(e);
-				super.scan(e, p);
-			}
-			return null;
-		}
-
-		@Override
-		public Object visitExecutable(ExecutableElement m, Object p) {
-			// Get the right module.
-			final ModuleSpecBuilder module = processedModules.get(m);
-
-			
-			TypeElement cls = (TypeElement)m.getEnclosingElement();
-			String sig = DescriptorWrangler.methodDescriptor(cls, m);
-			
-
-			assert module != null;
-
-			Reader readerAnn = m.getAnnotation(Reader.class);
-			Writer writerAnn = m.getAnnotation(Writer.class);
-			Inline inlineAnn = m.getAnnotation(Inline.class);
-			NonComm nonCommAnn = m.getAnnotation(NonComm.class);
-
-			if (((readerAnn != null || writerAnn != null) && (inlineAnn != null || nonCommAnn != null))
-					|| (inlineAnn != null && nonCommAnn != null)) {
-				error("method " + sig + ": conflicting annotations. @Inline, @NonComm, and @Writer/@Reader are mutually exclusive.");
-			}
-
-			boolean nonEmptyGroups = false;
-			// Group membership.
-			if (readerAnn != null) {
-				for (String groupId : readerAnn.value()) {
-					module.addReader(groupId, sig);
-					module.ctrGroupMembership++;
-				}
-				nonEmptyGroups = readerAnn.value() != null && readerAnn.value().length > 0;
-			}
-			if (writerAnn != null) {
-				for (String groupId : writerAnn.value()) {
-					module.addWriter(groupId, sig);
-					module.ctrGroupMembership++;
-				}
-				nonEmptyGroups = nonEmptyGroups || (writerAnn.value() != null && writerAnn.value().length > 0);
-			}
-
-			// Explicit Non-comm
-			if (nonCommAnn != null || ((writerAnn != null || readerAnn != null) && !nonEmptyGroups)) {
-				module.addNonComm(sig);
-			}
-
-			// Default (unannotated)
-			if (readerAnn == null && writerAnn == null && nonCommAnn == null && inlineAnn == null) {
-				module.addUnannotatedMethod(sig);
-			}
-
-			// Explicit Inline
-			if (inlineAnn != null) {
-				module.inlineMethod(sig);
-			}
-
-			
-			return null;
-		}
-
-		@Override
-		public Object visitPackage(PackageElement e, Object p) {
-			// TODO:  Allow cascading defaults?
-			return super.visitPackage(e, p);
-		}
-
-		@Override
-		public Object visitType(TypeElement e, Object p) {
-			// TODO:  Allow cascading defaults?
-			return super.visitType(e, p);
-		}
+	enum Comm { INLINE, NONCOMM, COMM, ERROR, DEFAULT };
+	class CommTraversal extends LabelingTraversal<CommTraversal.MethodSpec> {
+		// TODO: singletons for INLINE, NONCOMM
+		// TODO different subclasses for inline, noncomm, comm.  Merge method via dynamic dispatch + static overloading/casts.
+//		final InlineTag inlineTag = new InlineTag();
+//		final NonCommTag noncommTag = new NonCommTag();
+//	
+//		abstract class CommTag {
+//			
+//		}
+//		
+//		class InlineTag extends CommTag { }
+//		class NonCommTag { }
+//		
+//		class RWTag {
+//			final Set<ModuleSpecBuilder.Group> readGroups = new HashSet<ModuleSpecBuilder.Group>(), writeGroups = new HashSet<ModuleSpecBuilder.Group>();
+//		}
 		
+		class MethodSpec {
+			final Inline inline;
+			final NonComm noncomm;
+			final Reader reader;
+			final Writer writer;
+			final Comm comm;
+			public MethodSpec(Inline inline, NonComm noncomm, Reader reader, Writer writer) {
+				this.reader = reader;
+				this.writer = writer;
+				this.inline = inline;
+				this.noncomm = noncomm;
+				if (inline != null) {
+					if (noncomm == null && reader == null && writer == null) {
+						comm = Comm.INLINE;
+					} else {
+						comm = Comm.ERROR;
+					}
+				} else if (noncomm != null) {
+					if (reader == null && writer == null) {
+						comm = Comm.NONCOMM;
+					} else {
+						comm = Comm.ERROR;
+					}
+				} else if (reader != null || writer != null) {
+					comm = Comm.COMM;
+				} else {
+					comm = Comm.DEFAULT;
+				}
+			}
+			public MethodSpec(Comm c) {
+				inline = null;
+				noncomm = null;
+				reader = null;
+				writer = null;
+				comm = c;
+			}
+			public String toString() {
+				return "" + inline + noncomm + reader + writer + "    " + comm;
+			}
+		}
+
+		private final Map<Element, MethodSpec> defaultMethodSpecs = new HashMap<Element, MethodSpec>();
+
+		@Override
+		protected MethodSpec getLabelFromAnnotation(Element e) {
+			MethodSpec ms = new MethodSpec(e.getAnnotation(Inline.class), e.getAnnotation(NonComm.class),
+					e.getAnnotation(Reader.class), e.getAnnotation(Writer.class));
+			if (ms.comm == Comm.ERROR) {
+				error(e.getSimpleName() + ": conflicting annotations. @Inline, @NonComm, and @Writer/@Reader are mutually exclusive.");
+			}
+			return ms;
+		}
+
+		@Override
+		protected MethodSpec mergeLabels(MethodSpec label, MethodSpec parentLabel) {
+			if (label == null) return parentLabel;
+			if (parentLabel == null) return label;
+			switch (label.comm) {
+			case INLINE:
+			case NONCOMM:
+				return label;
+			case COMM:
+				if ((parentLabel.reader != null && label.reader == null) || (parentLabel.writer != null && label.writer == null)) {
+					return new MethodSpec(null, null, label.reader != null ? label.reader : parentLabel.reader,
+							label.writer != null ? label.writer : parentLabel.writer);
+				} else {
+					return label;
+				}
+			case DEFAULT:
+				return defaultAnnotation();
+			default:
+				return null;
+			}
+		}
+
+		@Override
+		protected void setLabel(Element e, MethodSpec r) {
+			defaultMethodSpecs.put(e, r);
+			if (e instanceof ExecutableElement) {
+				final ExecutableElement m = (ExecutableElement)e;
+				// Get the right module.
+				final ModuleSpecBuilder module = processedModules.get(m);
+				TypeElement cls = (TypeElement)e.getEnclosingElement();
+				String sig = DescriptorWrangler.methodDescriptor(cls, m);
+
+				boolean nonEmptyGroups = false;
+				// Group membership.
+				if (r.reader != null) {
+					for (String groupId : r.reader.value()) {
+						module.addReader(groupId, sig);
+						module.ctrGroupMembership++;
+					}
+					nonEmptyGroups = r.reader.value() != null && r.reader.value().length > 0;
+				}
+				if (r.writer != null) {
+					for (String groupId : r.writer.value()) {
+						module.addWriter(groupId, sig);
+						module.ctrGroupMembership++;
+					}
+					nonEmptyGroups = nonEmptyGroups || (r.writer.value() != null && r.writer.value().length > 0);
+				}
+
+				// Explicit Non-comm
+				if (r.noncomm != null || ((r.writer != null || r.reader != null) && !nonEmptyGroups)) {
+					module.addNonComm(sig);
+				}
+
+				// Default (unannotated)
+				if (r.reader == null && r.writer == null && r.noncomm == null && r.inline == null) {
+					module.addUnannotatedMethod(sig);
+				}
+
+				// Explicit Inline
+				if (r.inline != null) {
+					module.inlineMethod(sig);
+				}
+
+			}		
+		}
+
+		@Override
+		protected MethodSpec getLabel(Element e) {
+			return defaultMethodSpecs.get(e);
+		}
+
+		@Override
+		public MethodSpec visitPackage(PackageElement e, Object _) {
+			return defaultAnnotation();
+		}
+		private MethodSpec defaultAnnotation() {
+			return ModuleSpecBuilder.DEFAULT_INLINE ? new MethodSpec(Comm.INLINE) : new MethodSpec(Comm.NONCOMM);
+		}
 	}
 	
 	private void dumpChanges() {
